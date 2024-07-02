9690bbcbd57a9c3f4da5cd1e8c0b87ed09174bdd
javilenzuelac <ja.valenzuelac@duocuc.cl>
2024-07-02 15:50:52 +0000

    Archivos  examen transversal

---
+++ integrantes.py
@@ -0,0 +1,15 @@
+# lista_integrantes.py
+
+integrantes = ["Javiera Valenzuela"]
+
+def imprimir_integrantes(integrantes):
+    print("Integrantes:")
+    for integrante in integrantes:
+        print(integrante)
+
+if __name__ == "__main__":
+    imprimir_integrantes(integrantes)
+
+
+
+
